da0116857a087a751f9de688c660d6e37ccdc2c8
stuart7j <lubwamajohnstuart201@gmail.com>
2023-08-04 11:47:45 +0300

    first commit

---
+++ src/main/java/com/omnitech/mobilesyncservice/MobileSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omnitech.mobilesyncservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileSyncServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnitech/mobilesyncservice/MobileSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnitech.mobilesyncservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileSyncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
